fc0923a41fb73513fc49ba8e900690bece9da853
shintaisti21 <90827764+shintaisti21@users.noreply.github.com>
2021-09-19 13:33:35 +0700

    Add files via upload

---
+++ nomortiga.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="BIOGRAFI"/>
+        <meta name="Keywords" content="BIOGRAFI"/>
+        <meta name="authors" content="Shinta"/>
+        <meta charset="UTF-8"/>
+
+        <title>Daftar Riwayat Hidup </title>
+        <link rel="stylesheet" href="cssku.css"/>
+    </head>
+    <body>
+        <form action="#" style="width: 700px"class="posisi";>
+        <fieldset class="h"/>
+        <table style="width: 700px;">
+            <tr>
+                <td rowspan="10" width="50px">
+                <img src="shinta.JPEG" width="150px" height="150px"/>
+                </td>
+            </tr>
+            <tr>
+                <td><b>NIM</b></td>
+                <td>:</td>
+                <td>192520042</td>
+            </tr>
+            <tr>
+                <td><b>Nama Lengkap</b></td>
+                <td>:</td>
+                <td>Shinta Isti Arfiana</td>
+            </tr>
+            <tr>
+                <td><b>Alamat</b></td>
+                <td>:</td>
+                <td>Banjaran RT 02 RW 02, Tegalgot, Kepil, wonosobo</td>
+            </tr>
+            <tr>
+                <td><b>Bidang</b></td>
+                <td>:</td>
+                <td>Teknologi Informasi</td>
+            </tr>
+    </table>
+    </fieldset>
+    </form>
+</body>
+</html>
